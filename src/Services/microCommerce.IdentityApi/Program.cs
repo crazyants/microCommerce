f5064c60ab2c100aba306b088078109f49f7e709
Sefa Can <fsefacan@gmail.com>
2018-04-08 11:25:22 +0300

    upgraded to Microsoft.AspNetCore.All 2.0.6 & added Identity and Setting Api

---
+++ src/Services/microCommerce.IdentityApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace microCommerce.IdentityApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+            .Build();
+
+            new WebHostBuilder()
+            .UseConfiguration(config)
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build()
+            .Run();
+        }
+    }
+}
---
+++ src/Services/microCommerce.SettingApi/Startup.cs
@@ -0,0 +1,42 @@
+﻿using microCommerce.Mvc.Builders;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace microCommerce.SettingApi
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            return services.ConfigureApiServices(Configuration);
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.ConfigureApiPipeline(env);
+        }
+    }
+}
